8eb834852de7c451c3ed8c7bf5aee38bdf566e2e
Jalil <jalilbengoufa@gmail.com>
2017-09-29 22:11:35 -0400

    Delete config.php

--- Client/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'db681190790.db.1and1.com');
-   define('DB_USERNAME', 'dbo681190790');
-   define('DB_PASSWORD', 'Djalil1358851');
-   define('DB_DATABASE', 'db681190790');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
